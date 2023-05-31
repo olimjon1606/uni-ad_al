77c343d092206d650653baffcf7998f8089bfdc6
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-31 23:56:53 +0200

    Update brute_force.h

--- brute_force.h
+++ brute_force.h
@@ -1,9 +1,7 @@
 #ifndef BRUTE_FORCE_H
 #define BRUTE_FORCE_H
-
 #include <string>
 #include <vector>
 
 std::vector<int> bruteForce(const std::string& text, const std::string& pattern);
-
-#endif // BRUTE_FORCE_H
+#endif 
